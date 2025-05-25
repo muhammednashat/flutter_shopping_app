32948083398de86e6447d295bd2457d2ea07febb
muhammednashat <mnashat.dev@gmail.com>
2025-05-25 12:27:43 +0300

    init

---
+++ shopping_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/myapp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
---
+++ shopping_app/lib/ui/shop/ratings_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RatingsScreen extends StatelessWidget {
+  const RatingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
